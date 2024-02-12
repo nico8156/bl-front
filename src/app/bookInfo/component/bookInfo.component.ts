545a63fc59a785d26e08ea19f47bd9c32e9a3098
nico8156 <119879294+nico8156@users.noreply.github.com>
2024-02-12 18:26:09 +0100

    feature saving book to B

--- src/app/bookInfo/component/bookInfo.component.ts
+++ src/app/bookInfo/component/bookInfo.component.ts
@@ -7,12 +7,14 @@ import { selectBookInfodata, selectError, selectIsLoading } from "../store/reduc
 import { CommonModule } from "@angular/common";
 import { BookInfoService } from "../service/bookInfo.service";
 import { selectLinksData } from "../../shared/component/navlink/store/reducers";
+import { selectCurrentUser } from "../../auth/store/reducers";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     selector:'bl-bookInfo',
     templateUrl:'./bookInfo.component.html',
     standalone: true,
-    imports: [CommonModule, RouterLink]
+    imports: [CommonModule, RouterLink, FormsModule]
 })
 
 export class BookInfoComponent implements OnInit{
@@ -21,20 +23,42 @@ export class BookInfoComponent implements OnInit{
     bookInfoService = inject(BookInfoService)
 
     slug = this.route.snapshot.paramMap.get('slug') ?? ''
+    selectedLibrary = ''
+    bookToSendToDb = {}
+    userId = 0
+    libraryId = 0
 
     data$ = combineLatest({
         isLoading: this.store.select(selectIsLoading),
         error: this.store.select(selectError),
         book: this.store.select(selectBookInfodata),
-        links: this.store.select(selectLinksData)
+        links: this.store.select(selectLinksData),
+        user: this.store.select(selectCurrentUser)
       })
     
     ngOnInit(): void {
         this.store.dispatch(bookInfoActions.getBookInfo({slug: this.slug}))
     }
-    saveBook(){
+    saveBook(libraryName: string){
         
-        
+        console.log(libraryName)
+        this.data$.subscribe(data => {
+            this.bookToSendToDb = {
+                googleId : data.book?.id,
+                title: data.book?.volumeInfo.title,
+                authors: data.book?.volumeInfo.authors?.toString(),
+                publisher: data.book?.volumeInfo.publisher,
+                description: data.book?.volumeInfo.pageCount,
+                imageLinks : data.book?.volumeInfo.imageLinks?.thumbnail
+            }
+            const X = data.links?.filter(lib => lib.libraryName === libraryName) ?? []
+            this.libraryId = X[0].libraryId
+            console.log(this.libraryId)
+            this.userId = data.user?.userId ?? 0
+            console.log(this.userId)
+
+        })
+        console.log(this.bookToSendToDb)
         
     }
 }
